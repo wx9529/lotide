270095c3fc096a27706876fdedeba7d2508c67c3
wangxin <xinwang213z@gmail.com>
2022-03-09 22:53:41 +0000

    findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+const assertEqual = function (actual, expected) {
+  let flag = "";
+  let emoj = "";
+  let compare = "";
+  if (actual === expected) {
+    flag = "Passed";
+    emoj = "✅✅✅";
+    compare = "===";
+  } else {
+    flag = "Failed";
+    emoj = "🛑🛑🛑";
+    compare = "!==";
+  }
+  console.log(
+    `${emoj} Assertion ${flag}: [${actual}] ${compare} [${expected}]`
+  );
+};
+
+const findKeyByValue = function (obj, value) {
+  for (const key in obj) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
