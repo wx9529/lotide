c593fe1bcd1db37e2fe428177d351c84230aff52
wangxin <xinwang213z@gmail.com>
2022-03-14 19:31:36 +0000

    Refactor assertArrayEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function (arr1, arr2) {
   let flag = "";
@@ -28,7 +17,4 @@ const assertArraysEqual = function (arr1, arr2) {
   console.log(`${emoj} Assertion ${flag}: [${arr1}] ${compare} [${arr2}]`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([], [1, 2, 3]);
-assertArraysEqual([], []);
-assertArraysEqual(["2", "fasd"], ["2", "fasd"]);
+module.exports = assertArraysEqual;
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -5,3 +5,5 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 1);
 assertEqual(1, 33);
+
+
