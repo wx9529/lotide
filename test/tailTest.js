c2cf362dfbdd90aadb840cd5af8c108f75ee3309
wangxin <xinwang213z@gmail.com>
2022-03-14 20:37:54 +0000

    s

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,16 @@
-const tail = require('../tail')
-const assertEqual = require('../assertEqual')
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+const assert = require("chai").assert;
+const tail = require("../tail");
+
+describe("#tail", () => {
+  it("return ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), [
+      "Lighthouse",
+      "Labs",
+    ]);
+  });
+});
+
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
