fdba2f67cb616ed719f907f40a17b0fbca1eeefd
wangxin <xinwang213z@gmail.com>
2022-03-14 19:47:53 +0000

    refactor

--- middle.js
+++ middle.js
@@ -1,34 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  let flag = "";
-  let emoj = "";
-  let compare = "";
-
-  if (eqArrays(arr1, arr2)) {
-    flag = "Passed";
-    emoj = "✅✅✅";
-    compare = "===";
-  } else {
-    flag = "Failed";
-    emoj = "🛑🛑🛑";
-    compare = "!==";
-  }
-  console.log(`${emoj} Assertion ${flag}: [${arr1}] ${compare} [${arr2}]`);
-};
-
-const middle = function(array) {
+const middle = function (array) {
   if (array.length <= 2) {
     return [];
   } else {
@@ -44,8 +14,4 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
