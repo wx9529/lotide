a2152fef1650eb353faa4723d97fec3886f0be29
wangxin <xinwang213z@gmail.com>
2022-03-14 20:50:51 +0000

    s

--- test/tailTest.js
+++ test/tailTest.js
@@ -9,8 +9,3 @@ describe("#tail", () => {
     ]);
   });
 });
-
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
